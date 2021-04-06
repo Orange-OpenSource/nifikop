8432a56d36962cfa8be6726e0708a726ae18d013
Richard Peres <richard.peres@orange.com>
2021-04-06 11:37:48 +0200

    Add Get TLS Config logic & Tests from cert-manager implementation
    Signed-off-by: Richard Peres <richard.peres@orange.com>

--- pkg/pki/selfmanagerpki/selfmanager_tls_config.go
+++ pkg/pki/selfmanagerpki/selfmanager_tls_config.go
@@ -1,8 +1,61 @@
 package selfmanagerpki
 
-import "crypto/tls"
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"github.com/Orange-OpenSource/nifikop/api/v1alpha1"
+	"github.com/Orange-OpenSource/nifikop/pkg/errorfactory"
+	pkicommon "github.com/Orange-OpenSource/nifikop/pkg/util/pki"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"strings"
+)
 
-// TODO
-func (s SelfManager) GetControllerTLSConfig() (*tls.Config, error) {
-	panic("implement me")
+// GetControllerTLSConfig creates a TLS config from the user secret created for
+// cruise control and manager operations
+func (s *SelfManager) GetControllerTLSConfig() (config *tls.Config, err error) {
+	config = &tls.Config{}
+	tlsKeys := &corev1.Secret{}
+	err = s.client.Get(context.TODO(),
+		types.NamespacedName{
+			Namespace: s.cluster.Namespace,
+			Name:      fmt.Sprintf(pkicommon.NodeControllerTemplate, s.cluster.Name),
+		},
+		tlsKeys,
+	)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			err = errorfactory.New(errorfactory.ResourceNotReady{}, err, "controller secret not found")
+		}
+		return
+	}
+	clientCert := tlsKeys.Data[corev1.TLSCertKey]
+	clientKey := tlsKeys.Data[corev1.TLSPrivateKeyKey]
+	caCert := tlsKeys.Data[v1alpha1.CoreCACertKey]
+
+	if len(caCert) == 0 {
+		certs := strings.SplitAfter(string(clientCert), "-----END CERTIFICATE-----")
+		clientCert = []byte(certs[0])
+		caCert = []byte(certs[len(certs)-1])
+		if len(certs) == 3 {
+			caCert = []byte(certs[len(certs)-2])
+		}
+	}
+
+	x509ClientCert, err := tls.X509KeyPair(clientCert, clientKey)
+	if err != nil {
+		err = errorfactory.New(errorfactory.InternalError{}, err, "could not decode controller certificate")
+		return
+	}
+
+	rootCAs := x509.NewCertPool()
+	rootCAs.AppendCertsFromPEM(caCert)
+
+	config.Certificates = []tls.Certificate{x509ClientCert}
+	config.RootCAs = rootCAs
+
+	return
 }
--- pkg/pki/selfmanagerpki/selfmanager_tls_config_test.go
+++ pkg/pki/selfmanagerpki/selfmanager_tls_config_test.go
@@ -1,10 +1,53 @@
 package selfmanagerpki
 
 import (
+	"context"
+	"github.com/Orange-OpenSource/nifikop/api/v1alpha1"
+	"github.com/Orange-OpenSource/nifikop/pkg/errorfactory"
+	certutil "github.com/Orange-OpenSource/nifikop/pkg/util/cert"
+	corev1 "k8s.io/api/core/v1"
+	"reflect"
 	"testing"
 )
 
-// TODO
-func TestGetControllerTLSConfig(t *testing.T) {
+func newMockControllerSecret(valid bool) *corev1.Secret {
+	secret := &corev1.Secret{}
+	secret.Name = "test-controller"
+	secret.Namespace = "test-namespace"
+	cert, key, _, _ := certutil.GenerateTestCert()
+	if valid {
+		secret.Data = map[string][]byte{
+			corev1.TLSCertKey:       cert,
+			corev1.TLSPrivateKeyKey: key,
+			v1alpha1.CoreCACertKey:  cert,
+		}
+	}
+	return secret
+}
 
+func TestGetControllerTLSConfig(t *testing.T) {
+	manager := newMock(newMockCluster())
+
+	// Test good controller secret
+	manager.client.Create(context.TODO(), newMockControllerSecret(true))
+	if _, err := manager.GetControllerTLSConfig(); err != nil {
+		t.Error("Expected no error, got:", err)
+	}
+
+	manager = newMock(newMockCluster())
+
+	// Test non-existent controller secret
+	if _, err := manager.GetControllerTLSConfig(); err == nil {
+		t.Error("Expected error got nil")
+	} else if reflect.TypeOf(err) != reflect.TypeOf(errorfactory.ResourceNotReady{}) {
+		t.Error("Expected not ready error, got:", reflect.TypeOf(err))
+	}
+
+	// Test invalid controller secret
+	manager.client.Create(context.TODO(), newMockControllerSecret(false))
+	if _, err := manager.GetControllerTLSConfig(); err == nil {
+		t.Error("Expected error got nil")
+	} else if reflect.TypeOf(err) != reflect.TypeOf(errorfactory.InternalError{}) {
+		t.Error("Expected internal error, got:", reflect.TypeOf(err))
+	}
 }
